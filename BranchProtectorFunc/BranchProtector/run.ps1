dee3959955c2794ff13dce09ee501286962e1775
Assaf Stone <asstone@microsoft.com>
2022-05-02 00:24:26 -0400

    Clean up of code

--- BranchProtectorFunc/BranchProtector/run.ps1
+++ BranchProtectorFunc/BranchProtector/run.ps1
@@ -11,7 +11,6 @@ Write-Host "PowerShell HTTP trigger function processed the request."
 $secret = [System.Environment]::GetEnvironmentVariable("ACCESS_TOKEN", [System.EnvironmentVariableTarget]::Process)
 
 if ($TriggerMetadata) {
-    # $client_payload = $TriggerMetadata | ConvertFrom-Json -Depth 5
     Write-Host "Action: $($TriggerMetadata.action)"
 
     if ($TriggerMetadata.action -eq "created") {
@@ -22,35 +21,10 @@ if ($TriggerMetadata) {
 
         Write-Host "Dispatching ensure-reviews workflow"
         Start-RepoProtectionWorkflow -Username $senderLogin -Token $secret -Owner $owner -Repo $repo -Branch $branch
-
-
-        # Write-Host "Setting policy for $owner/$repo : $branch"
-
-        # $protectionTemplate = (Get-BranchProtection -Username $senderLogin -Token $secret -Owner $owner -Repo Template -Branch main)
-
-        # Write-Host "Branch protection template: $protectionTemplate"
-
-        # Set-DefaultBranchContent -Username $senderLogin -Token $secret -Owner $owner -Repo $repo
-
-        # $responseCode = Set-BranchProtection -Username $senderLogin -Token $secret -Owner $owner -Repo Template -Branch main -ProtectionTemplate $protectionTemplate
     }
 }
 
-
-
-
-
-# Interact with query parameters or the body of the request.
-$name = $Request.Query.Name
-if (-not $name) {
-    $name = $Request.Body.Name
-}
-
-$body = "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-
-if ($name) {
-    $body = "Hello, $name. This HTTP triggered function executed successfully."
-}
+$body = "This HTTP triggered function executed successfully."
 Write-Host $body
 
 # Associate values to output bindings by calling 'Push-OutputBinding'.
